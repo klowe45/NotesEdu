e8966c0dfd866f144cb15f01e8463ae2f6be9dce
Kenneth Lowe <kennethlowe45@yahoo.COM>
2025-10-03 17:25:01 -0500

    PostgresSql added

---
+++ PG-NotesEdu.session.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS students (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    middle_name VARCHAR(100),
+    last_name VARCHAR(100),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
